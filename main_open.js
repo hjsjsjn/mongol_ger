68f3542798c4667ebfb0206e08d4a711a6661011
ujin <burteganbayr@gmail.com>
2025-12-27 15:25:13 +0800

    Update main_open.js

--- main_open.js
+++ main_open.js
@@ -1,6 +1,6 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
+import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/controls/OrbitControls.js";
 
 const container = document.getElementById('canvasWrap');
 const showAllBtn = document.getElementById('showAll');
@@ -208,4 +208,5 @@ function animate() {
 }
 
 animate();
+
 
