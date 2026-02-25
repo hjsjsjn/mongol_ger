707f329196505747d36601f104724551c5c5a2b3
ujin <burteganbayr@gmail.com>
2026-02-25 20:31:03 +0800

    Update togloom.js

--- togloom.js
+++ togloom.js
@@ -1,10 +1,8 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160/build/three.module.js";
+import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
+import { OrbitControls } from "https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://unpkg.com/three@0.160.0/examples/jsm/loaders/GLTFLoader.js";;
 
-import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.160/examples/jsm/controls/OrbitControls.js";
-
-import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.160/examples/jsm/loaders/GLTFLoader.js";
-
-import { TransformControls } from "https://cdn.jsdelivr.net/npm/three@0.160/examples/jsm/controls/TransformControls.js";
+import { TransformControls } from "https://unpkg.com/three@0.160/examples/jsm/controls/TransformControls.js";
 
 const parts = [
   { id: 'hana', file: 'models/hana.glb', name:"Хана" },
@@ -417,6 +415,7 @@ miniOpenBtn.addEventListener("click", () => {
     leftCol.classList.remove("collapsed");
     miniOpenBtn.style.display = "none";
 });
+
 
 
 
